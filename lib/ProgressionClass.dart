0264b0f3bdb036ec0a3a829f05083d2c8ef22782
Haywire-d <79108425+Haywire-d@users.noreply.github.com>
2021-03-29 19:17:50 -0400

    Ajout des pages
    Le design est à modifier

---
+++ lib/ProgressionClass.dart
@@ -0,0 +1,35 @@
+class Progression {
+  String id = '';
+  String metier= '';
+  String metier_rep= '';
+  String equipement= '';
+  String equipement_rep= '';
+  String tache= '';
+  String tache_rep= '';
+  String individu= '';
+  String individu_rep= '';
+  String envrionnement= '';
+  String envrionnement_rep= '';
+  String ressource= '';
+  String ressource_rep= '';
+
+  Progression({this.id, this.metier, this.metier_rep, this.equipement, this.equipement_rep, this.tache, this.tache_rep, this.individu, this.individu_rep, this.envrionnement, this.envrionnement_rep, this.ressource, this.ressource_rep});
+
+  factory Progression.fromJson(Map<String, dynamic> json) {
+    return Progression(
+        id: json['id'] as String,
+        metier: json['metier'] as String,
+        metier_rep: json['metier_rep'] as String,
+        equipement: json['equipement'] as String,
+        equipement_rep: json['equipement_rep'] as String,
+        tache: json['tache'] as String,
+        tache_rep: json['tache_rep'] as String,
+        individu: json['individu'] as String,
+        individu_rep: json['individu_rep'] as String,
+        envrionnement: json['envrionnement'] as String,
+        envrionnement_rep: json['envrionnement_rep'] as String,
+        ressource: json['ressource'] as String,
+        ressource_rep: json['ressource_rep'] as String
+    );
+  }
+}
