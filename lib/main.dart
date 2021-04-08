7fd553d93b48b068664a23a087a615e79fd8492e
mHammache <79108709+mHammache@users.noreply.github.com>
2021-04-08 16:01:24 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'PageLogin.dart';
-import 'Services.dart';
-import 'Utilisateur.dart';
-import 'PageProfil.dart';
-import 'PageProgression.dart';
-import 'PageCreationProfil.dart';
-import 'PageListeEleves.dart';
-
+//application de base pour mettre sur GitHub
+//testing yep
+//ok le projet est modifiable et fonctionne!
+//sauce brune
+// hey
 void main() {
   runApp(MyApp());
 }
@@ -15,61 +12,106 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return RestartWidget(
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          routes: <String, WidgetBuilder>{
-            '/creationProfile': (context) => new PageCreationProfile(null),
-            '/login': (context) => new PageLogin(),
-            '/listeEleves': (context) => new PageListeEleves(null),
-            '/progression': (context) => new PageProgression(null),
-            '/profil': (context) => new PageProfil(null),
-            '/aide': (context) => new PageProgression(null),
-          },
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            brightness: Brightness.dark,
-            primarySwatch: Colors.blueGrey,
-            fontFamily: 'Georgia',
-            textTheme: TextTheme(
-              headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-              headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-              bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-            ),
-          ),
-          initialRoute: '/login',
-        ));
+    return MaterialApp(
+      title: 'Flutter Demo allo yo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-class RestartWidget extends StatefulWidget {
-  RestartWidget({this.child});
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final Widget child;
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-  static void restartApp(BuildContext context) {
-    context.findAncestorStateOfType<_RestartWidgetState>().restartApp();
-  }
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _RestartWidgetState createState() => _RestartWidgetState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _RestartWidgetState extends State<RestartWidget> {
-  Key key = UniqueKey();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  void restartApp() {
+  void _incrementCounter() {
     setState(() {
-      key = UniqueKey();
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return KeyedSubtree(
-      key: key,
-      child: widget.child,
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
