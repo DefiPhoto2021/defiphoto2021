c27fbebd760aa6c23abdeff7673cedaaed428249
JayBernard01 <79108625+JayBernard01@users.noreply.github.com>
2021-04-19 13:59:07 -0400

    fichier_PHP_Join

---
+++ lib/join.php
@@ -0,0 +1,11 @@
+<?php 
+//mon but c'est de faire une fonction qui permet d'associer les tables pour les choses qu'ils ont en commun 
+//- associer le ID à toutes les bases de données:
+//- eleves à prof
+//- progression à l'individu
+//- questions au prof et à l'élève
+
+
+
+
+?>
